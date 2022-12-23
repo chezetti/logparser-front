8740f269ad82acd933b5e5544e388d399c07b9a1
Izmailov, Riza <riza.izmailov@itglobal.com>
2022-12-23 14:45:41 +0300

    upd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+interface IOccuranceFrequency {
+  [level: string]: {
+    [date: string]: number;
+  };
+}
 
 export default function PrivatePage() {
   const [file, setFile] = useState(null);
   const [errorCount, setErrorCount] = useState(0);
   const [infoCount, setInfoCount] = useState(0);
+  const [warnCount, setWarnCount] = useState(0);
+  const [levelCountsByTimestamp, setlevelCountsByTimestamp] = useState<IOccuranceFrequency>({});
+  const [levelCountsByTimestampData, setlevelCountsByTimestampData] = useState({});
 
   const uploadToClient = (event: any) => {
     if (event.target.files && event.target.files[0]) {
@@ -16,7 +27,7 @@ export default function PrivatePage() {
   const uploadToServer = async (event: any) => {
     const formData = new FormData();
     formData.append("file", file!);
-
+    
     const requestOptions = {
       method: 'POST',
       body: formData,
@@ -25,12 +36,79 @@ export default function PrivatePage() {
     fetch("http://localhost:9000/parser", requestOptions)
       .then(response => response.json())
       .then((data) => {
-        setErrorCount(data.errorCount);
-        setInfoCount(data.infoCount);
+        setErrorCount(data.levelInfo.errorCount);
+        setInfoCount(data.levelInfo.infoCount);
+        setWarnCount(data.levelInfo.warnCount);
+
+        setlevelCountsByTimestamp(data.occuranceFrequency.levelCountsByTimestamp);
+
+        const timestamps = [
+          new Set([
+            ...Object.keys(levelCountsByTimestamp.INFO),
+            ...Object.keys(levelCountsByTimestamp.ERROR),
+            ...Object.keys(levelCountsByTimestamp.WARN),
+          ]),
+        ];
+        
+      
+        const levelCountsByTimestampData = {
+          labels: timestamps,
+          datasets: [
+            {
+              label: 'INFO',
+              data: [...Object.values(levelCountsByTimestamp.INFO)],  
+              borderColor: 'rgba(255, 99, 132, 1)',
+              pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
+            },
+            {
+              label: 'ERROR',
+              data: [ ...Object.values(levelCountsByTimestamp.ERROR) ],
+              borderColor: 'rgba(54, 162, 235, 1)',
+              pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(54, 162, 235, 1)',
+            },
+            {
+              label: 'WARNING',
+              data: [...Object.values(levelCountsByTimestamp.INFO)],
+              borderColor: 'rgba(255, 206, 86, 1)',
+              pointBackgroundColor: 'rgba(255, 206, 86, 1)',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: 'rgba(255, 206, 86, 1)',
+            },
+          ],
+        }; 
+        setlevelCountsByTimestampData(levelCountsByTimestampData);
       })
       .catch(error => console.log('error', error));
   };
 
+  const levelCountData = {
+    labels: ['Errors', 'Infos', 'Warns'],
+    datasets: [
+      {
+        label: 'Level Count',
+        data: [errorCount, infoCount, warnCount],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
+  
   return (
     <div>
       <div>
@@ -44,7 +122,16 @@ export default function PrivatePage() {
           Parse
         </button>
         <p></p>
-        <div>Errors: {errorCount}, Infos: {infoCount}</div>
+        <div>Errors: {errorCount}, Infos: {infoCount}, Warns: {warnCount}</div>
+        <h2>Log Level Count Bar</h2>
+        <Bar
+          data={levelCountData}
+        />
+        <h2>Level Counts By Timestamp Bar</h2>
+        <Bar
+          data={levelCountsByTimestampData} 
+          // options={levelCountsByTimestampOptions}
+        />
       </div>
     </div>
   );
