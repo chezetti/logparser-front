cf2573da7ba537b7490a237515d2d2c78767613a
Riezowe Kawatashi <riza25@bk.ru>
2022-12-25 19:30:19 +0300

    upd

--- src/pages/components/Content.tsx
+++ src/pages/components/Content.tsx
@@ -1,14 +1,10 @@
-import { useState, useEffect } from "react";
-import { Bar, Doughnut, Pie } from 'react-chartjs-2';
-import styles from "../../../styles/Home.module.css";
+import { useState } from "react";
+import { Bar, Doughnut, Line, Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
-import { IParsedLog } from "./interfaces/parsed-log.interface";
 
-interface IOccuranceFrequency {
-    [level: string]: {
-        [date: string]: number;
-    };
-}
+import { IParsedLog } from "./interfaces/parsed-log.interface";
+import { IOccuranceFrequency } from "./interfaces/occurance-frequency.interface";
+import { APP } from "../../config/app.config";
 
 export default function PrivatePage() {
     const [file, setFile] = useState(null);
@@ -20,12 +16,11 @@ export default function PrivatePage() {
     const [shouldHide, setShouldHide] = useState(true);
 
     const [levelCountsByTimestamp, setlevelCountsByTimestamp] = useState<IOccuranceFrequency>({});
-    const [levelCountsByTimestampData, setlevelCountsByTimestampData] = useState({});
 
     const uploadToClient = (event: any) => {
         if (event.target.files && event.target.files[0]) {
             const file = event.target.files[0];
-            console.log(file.name);
+
             setFile(file);
         }
     };
@@ -39,7 +34,7 @@ export default function PrivatePage() {
             body: formData,
         };
 
-        fetch("http://localhost:9000/parser", requestOptions)
+        fetch(`${APP.BACKEND_URL}/parser`, requestOptions)
             .then(response => response.json())
             .then((data: IParsedLog) => {
                 setErrorCount(data.logLevelInfo.ERROR);
@@ -48,55 +43,106 @@ export default function PrivatePage() {
                 setTotalCount(data.logLevelInfo.TOTAL);
                 setShouldHide(false);
                 setlevelCountsByTimestamp(data.occuranceFrequency.levelCountsByTimestamp);
-
-                // const timestamps = [
-                //     new Set([
-                //         ...Object.keys(levelCountsByTimestamp.INFO),
-                //         ...Object.keys(levelCountsByTimestamp.ERROR),
-                //         ...Object.keys(levelCountsByTimestamp.WARN),
-                //     ]),
-                // ];
-
-
-                // const levelCountsByTimestampData = {
-                //     labels: timestamps,
-                //     datasets: [
-                //         {
-                //             label: 'INFO',
-                //             data: [...Object.values(levelCountsByTimestamp.INFO)],
-                //             borderColor: 'rgba(255, 99, 132, 1)',
-                //             pointBackgroundColor: 'rgba(255, 99, 132, 1)',
-                //             pointBorderColor: '#fff',
-                //             pointHoverBackgroundColor: '#fff',
-                //             pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
-                //         },
-                //         {
-                //             label: 'ERROR',
-                //             data: [...Object.values(levelCountsByTimestamp.ERROR)],
-                //             borderColor: 'rgba(54, 162, 235, 1)',
-                //             pointBackgroundColor: 'rgba(54, 162, 235, 1)',
-                //             pointBorderColor: '#fff',
-                //             pointHoverBackgroundColor: '#fff',
-                //             pointHoverBorderColor: 'rgba(54, 162, 235, 1)',
-                //         },
-                //         {
-                //             label: 'WARNING',
-                //             data: [...Object.values(levelCountsByTimestamp.INFO)],
-                //             borderColor: 'rgba(255, 206, 86, 1)',
-                //             pointBackgroundColor: 'rgba(255, 206, 86, 1)',
-                //             pointBorderColor: '#fff',
-                //             pointHoverBackgroundColor: '#fff',
-                //             pointHoverBorderColor: 'rgba(255, 206, 86, 1)',
-                //         },
-                //     ],
-                // };
-                // setlevelCountsByTimestampData(levelCountsByTimestampData);
             })
             .catch(error => console.log('error', error));
     };
 
+    const timestamps = () => {
+        if (Object.keys(levelCountsByTimestamp).length != 0) {
+            console.log(Object.keys(levelCountsByTimestamp).length);
+            return [
+                new Set([
+                    ...Object.keys(levelCountsByTimestamp.INFO),
+                    ...Object.keys(levelCountsByTimestamp.ERROR),
+                    ...Object.keys(levelCountsByTimestamp.WARN),
+                ]),
+            ];
+        }
+
+        return [];
+    }; 
+
+
+    const levelCountsByTimestampData = () => {
+        if (Object.keys(levelCountsByTimestamp).length != 0) {
+            return {
+                labels: Array.from(timestamps()[0]).sort(),
+                datasets: [
+                    {
+                        label: 'INFO',
+                        data: [...Object.values(levelCountsByTimestamp.INFO)],
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                        pointBorderColor: 'blue',
+                        pointHoverBackgroundColor: '#fff',
+                        pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
+                    },
+                    {
+                        label: 'ERROR',
+                        data: [...Object.values(levelCountsByTimestamp.ERROR)],
+                        borderColor: 'rgba(0, 0, 255, 1)', // blue
+                        pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                        pointBorderColor: '#red',
+                        pointHoverBackgroundColor: '#fff',
+                        pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
+                    },
+                    {
+                        label: 'WARN',
+                        data: [...Object.values(levelCountsByTimestamp.WARN)],
+                        borderColor: 'rgba(255, 206, 86, 1)',
+                        pointBackgroundColor: 'rgba(255, 206, 86, 1)',
+                        pointBorderColor: 'yellow',
+                        pointHoverBackgroundColor: '#fff',
+                        pointHoverBorderColor: 'rgba(255, 206, 86, 1)',
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(75,192,192,0.4)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                    },
+                ],
+            }
+        }
+        
+        return {
+            labels: [
+                'ERROR',
+                'INFO',
+                'WARN'
+            ],        
+            datasets: [
+                {
+                    label: 'Level Count',
+                    data: [errorCount, infoCount, warnCount],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1
+                }
+            ]
+        };
+    };
+
     const levelCountData = {
-        labels: ['Errors', 'Infos', 'Warns'],
+        labels: [
+            'ERROR',
+            'INFO',
+            'WARN'
+        ],        
         datasets: [
             {
                 label: 'Level Count',
@@ -205,11 +251,11 @@ export default function PrivatePage() {
                                 height={100}
                             />
                         </div>
-
-                        {/* <Bar
-                            data={levelCountsByTimestampData} 
-                            // options={levelCountsByTimestampOptions}
-                      /> */}
+                        <Line
+                        //TODO
+                        //@ts-ignore
+                            data={levelCountsByTimestampData()} 
+                        />
                         <div className="col-lg-5 col-12 text-center mb-5">
                             <h2>Level Counts By Timestamp Bar</h2>
                             <Doughnut
